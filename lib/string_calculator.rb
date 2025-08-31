7b9b03b07652e01569a8f802bd19a7d683c0a10e
Kalpesh Vasave <kalpeshvasave2@gmail.com>
2025-08-31 15:44:25 +0530

    code -> multiple delimiters with length more than 1

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -23,7 +23,7 @@ class StringCalculator
       elsif get_character_groups_count(custom_delimiter) == 1
         delimiters.push(custom_delimiter[1..-2])
       else
-        delimiters += custom_delimiter.scan(/\[(.?)\]/).map(&:first)
+        delimiters += custom_delimiter.scan(/\[(.*?)\]/).map(&:first)
       end
     end
 
