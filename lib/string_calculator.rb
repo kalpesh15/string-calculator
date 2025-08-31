86ee5a96b2e59964a4f142ebaee47ca3965c6e67
Kalpesh Vasave <kalpeshvasave2@gmail.com>
2025-08-31 13:22:36 +0530

    code -> \n as delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -6,6 +6,6 @@ class StringCalculator
       return 0
     end
 
-    numbers.split(",").reduce(0) { |sum, number| sum + number.to_i }
+    numbers.split(/,|\n/).reduce(0) { |sum, number| sum + number.to_i }
   end
 end
