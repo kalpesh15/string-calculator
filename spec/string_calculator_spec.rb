4c76ab94780f69eac34133b271efcfd189b044c0
Kalpesh Vasave <kalpeshvasave2@gmail.com>
2025-08-31 13:01:35 +0530

    test -> two numbers input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,5 +14,9 @@ RSpec.describe StringCalculator do
     it "task a single number and returns the sum" do
       expect(calculator.add("1")).to eq(1)
     end
+
+    it "task two numbers and returns the sum" do
+      expect(calculator.add("1,2")).to eq(3)
+    end
   end
 end
