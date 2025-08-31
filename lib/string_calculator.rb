722214b427d01cb7598f4b7a017dad31e1f91916
Kalpesh Vasave <kalpeshvasave2@gmail.com>
2025-08-31 13:44:18 +0530

    code -> allow custom delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -6,6 +6,13 @@ class StringCalculator
       return 0
     end
 
+    if numbers.start_with?("//")
+      custom_delimiter, numbers = numbers.split("\n")
+      custom_delimiter = custom_delimiter[2..]
+  
+      return numbers.split(/#{custom_delimiter}/).reduce(0) { |sum, number| sum + number.to_i }
+    end
+
     numbers.split(/,|\n/).reduce(0) { |sum, number| sum + number.to_i }
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe StringCalculator do
       expect(calculator.add("1,2,3,10")).to eq(16)
     end
 
-    it "allows '\n' as well as ',' character between numbers and returns sum" do
+    it "allows '\\n' as well as ',' character between numbers and returns sum" do
       expect(calculator.add("1\n2,3")).to eq(6)
     end
 
